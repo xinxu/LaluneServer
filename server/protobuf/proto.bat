1979665faa9998f33bc62e0e06cf43a6763318e8
weiyuemin <weiyuemin@163.com>
2014-09-12 14:24:46 +0800

    不用那个src-gen文件夹了，麻烦

--- server/protobuf/proto.bat
+++ server/protobuf/proto.bat
@@ -1,1 +1,1 @@
-..\..\..\LaluneCommon\3rdparty\protobuf-2.6.0\vsprojects\Debug\protoc.exe --proto_path=./ %1 --cpp_out=./src-gen
+..\..\..\LaluneCommon\3rdparty\protobuf\vsprojects\Debug\protoc.exe --proto_path=./ %1 --cpp_out=./
