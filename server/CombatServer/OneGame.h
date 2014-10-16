e4f6695561cbe1a81c2f774663905705e128c938
Linux <weiyuemin@ue2game.com>
2014-10-16 16:39:28 +0800

    for linux compile

--- server/CombatServer/OneGame.h
+++ server/CombatServer/OneGame.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "CombateCommon.h"
-#include "NetLib\NetLib.h"
+#include "NetLib/NetLib.h"
 #include "include/ioservice_thread.h"
 #include "MessageTypeDef.h"
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
-#include "Log\Log.h"
+#include "Log/Log.h"
 #include <google/protobuf/stubs/common.h>
 #include "Battle.pb.h"
 #include "include/ptime2.h"
